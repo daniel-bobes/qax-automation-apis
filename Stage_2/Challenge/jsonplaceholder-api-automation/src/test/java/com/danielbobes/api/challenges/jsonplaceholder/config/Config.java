bf4c0d0268877567d9836eba97228b722c56970a
Daniel Bobes de Pedro <danibobesdepedro@gmail.com>
2026-02-11 14:39:40 +0100

    refactor: reorganización completa de paquetes a com.danielbobes.api y ajuste de jerarquía

---
+++ Stage_2/Challenge/jsonplaceholder-api-automation/src/test/java/com/danielbobes/api/challenges/jsonplaceholder/config/Config.java
@@ -0,0 +1,7 @@
+package com.danielbobes.api.challenges.jsonplaceholder.config;
+
+public class Config {
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    private Config() {}
+}
